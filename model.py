837c4347504fddb17e142e4f6874752602f61f0d
ashirwad khatri <khatriashirwad890@gmail.com>
2026-02-16 11:09:14 +0000

    Implemented Decision Tree model

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
 
@@ -16,10 +16,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
 
-model = LinearRegression()
+model = DecisionTreeRegressor(random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
 accuracy = r2_score(y_test, y_pred)
-print("Model Accuracy (R² Score):", accuracy)
+print("Decision Tree Accuracy (R² Score):", accuracy)
